df5d3a8d8a3df5ba6b14400d2cf7531fa4c0ea76
Louiza Achraf <achraf.louiza97@gmail.com>
2020-12-29 17:11:56 +0100

    Initialization of the project

---
+++ src/main/java/student/housing/ResidenceRental/ResidenceRentalApplication.java
@@ -0,0 +1,13 @@
+package student.housing.ResidenceRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidenceRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidenceRentalApplication.class, args);
+	}
+
+}
