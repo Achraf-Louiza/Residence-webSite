dfc8ab566155e316bf128f9361b7ebd4e5ca09d0
Louiza Achraf <achraf.louiza97@gmail.com>
2020-12-30 17:02:38 +0100

    Enabling CORS to Angular in port 4200

--- src/main/java/student/housing/ResidenceRental/ResidenceController.java
+++ src/main/java/student/housing/ResidenceRental/ResidenceController.java
@@ -1,6 +1,7 @@
 package student.housing.ResidenceRental;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,18 @@ public class ResidenceController {
     @Autowired
     public ResidenceController(ResidenceRepository residenceRepository){
         this.residenceRepository = residenceRepository;
-        residenceRepository.save(new Residence("R1", 100, 30, 400, 1000));
-        residenceRepository.save(new Residence("R2", 60, 9, 300, 765));
+        residenceRepository.save(new Residence("MIS pour jeunes actifs", 100, 30, 400, 1000));
+        residenceRepository.save(new Residence("August Rodin", 60, 9, 300, 765));
+        residenceRepository.save(new Residence("Quai de Loire", 26, 5, 508,896));
+        residenceRepository.save(new Residence("Le palatino", 11, 3, 411, 589));
+        residenceRepository.save(new Residence("Rive-gauche Tolbiac", 60, 9, 300, 765));
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value="/residences")
     public List<Residence> getResidences(){ return residenceRepository.findAll(); }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value="/residences/{nomResidence}")
     public Residence getCar(@PathVariable(value= "nomResidence" ) String nomResidence){
         for (Residence r : residenceRepository.findAll()){
