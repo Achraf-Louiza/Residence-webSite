5f5dfc29a1592192eddb24809b674c8c14619354
Abdelkarim Azzaz <Abdelkarim.azzaz@yahoo.com>
2021-01-05 21:20:21 +0100

    Adding form to reserve a studio

--- residenceRental/src/app/residence-detail/residence-detail.component.ts
+++ residenceRental/src/app/residence-detail/residence-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Residence } from '../residence';
 import { Studio } from '../studio';
+import { User } from '../user' ;
 import { ResidenceService } from '../residence.service';
 
 @Component({
@@ -11,7 +12,8 @@ import { ResidenceService } from '../residence.service';
 export class ResidenceDetailComponent implements OnInit {
   @Input() residence:Residence;
   studios: Studio[];
-
+  toReserve = false;
+  submitted = false;
 
   constructor(private residenceService: ResidenceService) {
 
@@ -25,6 +27,9 @@ export class ResidenceDetailComponent implements OnInit {
     this.residenceService.getStudiosWithObservable(this.residence?.nom).subscribe(studios => this.studios=studios);
   }
 
+  onClick(): void{
+  this.toReserve = true;
+  }
 
 
 }
---
+++ residenceRental/src/app/user.ts
@@ -0,0 +1,18 @@
+export class User {
+
+
+   constructor  (
+   public Nom: string ,
+   public Prenom: string ,
+   public Tel: number ,
+   public Email: string ,
+   public UserAdresse: string ,
+   public Choix: string ,
+   public Check: boolean ,
+
+   ) {}
+
+
+
+
+}
